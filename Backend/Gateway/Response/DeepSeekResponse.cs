d1f0953797fcb551153b32f5c88c366196439b17
Alejandro <naaxruk@gmail.com>
2025-07-04 12:08:45 -0600

    Se crearon varias clases nuevas, en el gateway igual

--- Backend/Gateway/Response/DeepSeekResponse.cs
+++ Backend/Gateway/Response/DeepSeekResponse.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gateway
+namespace Gateway.Response
 {
-    public class Class1
+    public class DeepSeekResponse
     {
     }
 }
