49001ffc8a3b232ecf22fcfa0092f6bde54aaf21
Alejandro <naaxruk@gmail.com>
2025-08-01 18:39:16 -0600

    Clases nuevas en Entity

---
+++ Backend/Entidades/Entity/Presupuesto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entity
+{
+    public class Presupuesto
+    {
+        public int BudgetID { get; set; }
+        public int UserID { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public decimal MaxAmount { get; set; }
+    }
+}
