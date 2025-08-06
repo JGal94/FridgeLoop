466fbe03f6d8e8a75ac4cb4b57228a4942a22308
Alejandro <naaxruk@gmail.com>
2025-08-06 11:13:06 -0600

    Se agregaron clases en Entidades

---
+++ Backend/Entidades/Response/ResRecetasIA.cs
@@ -0,0 +1,16 @@
+﻿using Entidades.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Response
+{
+    public class ResRecetasIA
+    {
+        public bool resultado { get; set; }
+        public List<Error> listaDeErrores { get; set; }
+        public List<Receta> recetas { get; set; }
+    }
+}
